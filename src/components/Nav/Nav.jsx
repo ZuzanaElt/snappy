d50e50ecfc929b67aaec2ec2e59af8cd4b687bae
ZuzanaElt <zuzanaeltom@gmail.com>
2023-03-23 17:34:03 +0000

    linked home button and logo to home page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,32 +1,37 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
+
 //import { FcHome } from '@react-icons/all-files/fa/FcHome';
 import housePic from './house.webp';
 import dinoLogo from './transparent.png';
+import '../../scss/components/nav.scss'
 
-var styles1 = {
-          
-  display: 'flexbox',
-  justifyContent: 'space-between',
-  margin: '10px 30px',
-};
-var styles2 = {
-    backgroundColor:'violet',
-    height: '60px',
-    display: 'inline-flex',
-    width:'100%',
-    justifyContent: 'space-between'
-  };
+
 
 export default function Nav({ profile, session }) {
   // if (profile) {
     return (
       <>
-      <div>
-          <div style={styles2}>
-            <img src={housePic} alt="home button" style={styles1}/>
-            <img src = {dinoLogo} alt="logo" style={styles1}/>
+        <div>
+          <div>
+            <div className= "nav-items">
+             
+                  <NavLink
+                            to="/"
+                            className="nav-home">
+                            
+                          <img src={housePic} alt="home button" className="home-button"/>
+                  </NavLink>
+                
+                  <NavLink
+                            to="/"
+                            className="nav-home">
+                          <img src = {dinoLogo} alt="logo" className="logo" />
+                  </NavLink>
+               
+            </div>
           </div>
-      <div>{profile ? <h2>Hi, {profile.username}</h2> : <p>Hello</p>}</div>
+        <div>{profile ? <h2>Hi, {profile.username}</h2> : <p>Hello</p>}</div>
       </div>
       
       </>
--- src/pages/LevelSelect.js
+++ src/pages/LevelSelect.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Card from '../../components/Card';
-import '../dist/level-select.css';
+import '../scss/pages/level-select.scss';
 
 const LevelSelect = () => {
     return (
