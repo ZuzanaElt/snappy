a9a7b74ce802f0f813b20b3ec41a5a7669ac7bec
unknown <dmlswebdev@gmail.com>
2023-03-23 16:51:42 +0200

    game pages added

---
+++ src/pages/CardMatchLevel3.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '../../components/Card';
+import '../dist/card-match-level-3.css';
+
+const CardMatchLevel3 = () => {
+    return (
+        <>
+          <h1>CardMatchLevel3 Page</h1>
+          <Card />
+          <Card />
+          <Card />
+        </>
+    );
+};
+
+export default CardMatchLevel3;
