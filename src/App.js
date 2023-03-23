3864ba4bc6160f907d384a6fa4da68c0da9bba9c
Sean Howe <seanmarchowe@gmail.com>
2023-03-23 14:22:40 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { useState, useEffect } from "react";
 import { supabase } from "../src/lib/supabaseClient";
-import AccountDisplay from "./pages/Account-View";
+import AccountDisplay from "./pages/Account";
 import Nav from "./components/Nav/Nav";
 
 function App() {
@@ -10,17 +15,20 @@ function App() {
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      async function getProfile() {
-        let { data, error } = await supabase
-          .from("profiles")
-          .select(`username, parentDOB, avatar_url`)
-          .eq("id", session.user.id)
-          .single();
-        return data;
+      console.log(session);
+      if (session) {
+        async function getProfile() {
+          let { data, error } = await supabase
+            .from("profiles")
+            .select(`username, parentDOB, avatar_url`)
+            .eq("id", session.user.id)
+            .single();
+          return data;
+        }
+        getProfile().then((data) => {
+          setProfile(data);
+        });
       }
-      getProfile().then((data) => {
-        setProfile(data);
-      });
       setSession(session);
     });
 
@@ -35,10 +43,14 @@ function App() {
         <Nav profile={profile} session={session} />
         <Routes>
           <Route path="/" />
-          <Route
-            path="/account"
-            element={<AccountDisplay session={session} />}
-          />
+          {session ? (
+            <Route
+              path="/account"
+              element={<AccountDisplay session={session} />}
+            />
+          ) : (
+            <Route path="/login" element={<Navigate to="/login" />} />
+          )}
         </Routes>
       </div>
     </Router>
