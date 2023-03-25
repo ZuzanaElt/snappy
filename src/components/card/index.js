e53b4ecaa603e9ee382c1a59a5493e1382cb2d6a
ZuzanaElt <zuzanaeltom@gmail.com>
2023-03-25 11:39:20 +0000

    handleClick added to flip cards

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, {  useState } from "react";
+import dino from "./dino.png"
+import house from "./house.webp";
+import ReactCardFlip from "react-card-flip";
 
 
+export default function Card({card,index}){
 
-export default function Card({ card, index }) {
-  return (
-    <>
-       <div>
-          <div className="card">
-            <div>
-            <img src="./background.png"/>
-             <p>{card}</p>
-            </div>
-            
+const[ flipped,setflipped] = useState(false); 
+
+const handleClick = () => {
+  setflipped(!flipped)
+}
+
+
+return(
+  <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+      <div className="">
+          <div className="">
+              <button className="back-flip-button card" onClick={handleClick}>
+                  <img className="card-back" alt="card-back" src={dino}/>
+              </button>
           </div>
       </div>
-    </>
-  );
+      <div className="">
+          <div className="">
+              <button className="back-flip-button card" onClick={handleClick}>
+                  <img className="card-back" alt="card-back" src={house}/>
+              </button>
+          </div>
+      </div>
+  </ReactCardFlip>
+
+)
 }
